c25aed0945a133abb541fba94fb13aa9283bcb29
Moinsky-sht <moinsky.sht@gmail.com>
2026-05-18 00:54:39 +0800

    fix: windowResizability placement

--- Sources/YouTubeToWAV/YouTubeToWAVApp.swift
+++ Sources/YouTubeToWAV/YouTubeToWAVApp.swift
@@ -5,9 +5,8 @@ struct YouTubeToWAVApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .windowResizability(.contentSize)
                 .frame(minWidth: 480, maxWidth: 480, minHeight: 280, maxHeight: 280)
         }
-        .windowResizability(.contentSize)
+        .windowResizability(.contentMinSize)
     }
 }
